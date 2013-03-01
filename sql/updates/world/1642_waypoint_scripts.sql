3fb6d829e9317fc6de3f5c5b5671050dc44535bd
Alex Grigoryev <evrial@users.noreply.github.com>
2013-03-01 19:56:48 +0400

    Fix error's spam in waypoint_scripts in outland

---
+++ sql/updates/world/1642_waypoint_scripts.sql
@@ -0,0 +1,2 @@
+UPDATE waypoint_scripts SET datalong2=1 WHERE datalong IN (35782, 32838, 33796, 28892, 292121, 29122, 29120, 33805, 33806, 33808);
+UPDATE creature_template SET AIName = "" WHERE entry = 21859;
